9ac601c1a48c24fe9711b9b7e5b0341fc1f4e810
Jessica Triana <jessica.triana.tx@gmail.com>
2019-12-18 18:35:53 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -1,3 +1,14 @@
 def using_push(array, string)
   array.push(string)
 end
+
+def using_unshift(array, string)
+  array.unshift(string)
+end
+
+def using_pop(array)
+  array.using_pop
+end
+
+def pop_with_args(array)
+  
